53335e65a83c73cec3fcac421274df31c090d7bd
Yashasvi <yashasvichilankar27@gmail.com>
2023-02-27 22:13:23 +0530

    Adding file BreakInteger

---
+++ BreakInteger.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class BreakInteger
+{
+int num;
+
+void setData()
+{
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Input six non negative Digits: ");
+num=sc.nextInt();
+}
+
+void getData()
+{
+int d1=num / 100000 % 10;
+int d2=num / 10000 % 10;
+int d3=num / 1000 % 10;
+int d4=num / 100 % 10;
+int d5=num / 10 % 10;
+int d6=num % 10;
+System.out.println(d1+" "+d2+" "+d3+" "+d4+" "+d5+" "+d6);
+}
+public static void main(String args[])
+{
+BreakInteger b1=new BreakInteger();
+b1.setData();
+b1.getData();
+}
+}
