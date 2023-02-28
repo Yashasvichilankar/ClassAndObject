5123f55a529b2e06f4ad71a72be0b4c85e8f01f7
Yashasvi <yashasvichilankar27@gmail.com>
2023-02-28 16:31:03 +0530

    Adding file Student.java

---
+++ Student.java
@@ -0,0 +1,15 @@
+class Student
+{
+int roll=2;
+String name="john";
+void show()
+{
+System.out.println("Name of Student:"+name+"\nRoll of Student: "+roll); 
+}
+public static void main(String args[])
+{
+Student s1=new Student();
+s1.show();
+}
+}
+
